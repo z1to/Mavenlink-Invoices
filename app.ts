7f28435402d71fdb57b58df846a79f7a49fbefb3
Miguel Jimenez <migueleduardo298@hotmail.com>
2021-04-02 17:13:25 -0400

    Set up eslint and prettier

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
-const port = 5000;
+const app = express()
+const port = 5000
 
 app.listen(port, () => {
-  console.log(`Running at http://localhost:${port}`);
-});
+  console.log(`Running at http://localhost:${port}`)
+})
