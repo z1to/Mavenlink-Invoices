1924974042a8b2a4416bb628e4a8b3daf8ef0a7b
dinasadir <45443818+dinasadir@users.noreply.github.com>
2021-04-10 17:10:05 -0400

    Basic Mavenlink Tasks routes
    - Added routes folder
    - Updated app.ts to add routes
    - Added api.ts
    - Added index.ts

--- service/src/app.ts
+++ service/src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import bodyparser from 'body-parser'
+import * as routes from './routes'
 
+dotenv.config()
 const app = express()
+routes.register(app);
 const port = 5000
 
 app.listen(port, () => {
