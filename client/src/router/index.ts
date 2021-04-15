fc69ea56ea555eadf9bece5692dadbe4526cc1d9
Miguel Jimenez <migueleduardo298@hotmail.com>
2021-04-15 16:45:02 -0400

    Create login view

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -15,6 +17,11 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
 ];
 
---
+++ client/src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+  </div>
+  <div id="login-form">
+    <input v-model="username" placeholder="user" />
+    <input v-model="password" placeholder="pass" />
+    <button @click="submit">Submit</button>
+  </div>
+</template>
+
+<script>
+import { Options, Vue } from "vue-class-component";
+
+function submit() {
+  alert("Hello " + this.username + "!");
+}
+
+@Options({
+  methods: {
+    submit,
+  },
+})
+export default class Login extends Vue {}
+</script>
