5b5e0ba8e7173a9b7e4e4e682a8802b95323d0a6
Miguel Jimenez <migueleduardo298@hotmail.com>
2021-04-15 18:21:37 -0400

    Enable cors for server and make login call from client using Axios

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -5,20 +5,49 @@
   <div id="login-form">
     <input v-model="username" placeholder="user" />
     <input v-model="password" placeholder="pass" />
-    <button @click="submit">Submit</button>
+    <button @click="login">Submit</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import qs from "qs";
+
 import { Options, Vue } from "vue-class-component";
 
-function submit() {
-  alert("Hello " + this.username + "!");
+async function login() {
+  const data = {
+    "username": this.username,
+    "password": this.password,
+  };
+
+  const options = {
+    method: "POST",
+    headers: { "content-type": "application/x-www-form-urlencoded", "Access-Control-Allow-Origin": "*" },
+    data: qs.stringify(data),
+    url: "http://localhost:5000/login",
+  };
+
+  let status, token, error;
+  axios(options)
+    .then((res) => {
+      status = res.status;
+      token = res.data.message;
+    })
+    .catch((err) => {
+        error = err;
+    });
 }
 
 @Options({
+  data() {
+    return {
+      username: "",
+      password: "",
+    }
+  },
   methods: {
-    submit,
+    login,
   },
 })
 export default class Login extends Vue {}
