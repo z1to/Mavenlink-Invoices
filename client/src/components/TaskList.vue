65855fde820c6229e889889eab0eeabc611916ec
Nicolas Zito <nicolasazito@gmail.com>
2021-04-20 16:35:34 -0400

    Updated Styling Added Buttons
    Updated Styling Added Buttons

--- client/src/components/TaskList.vue
+++ client/src/components/TaskList.vue
@@ -4,17 +4,17 @@
       <div class="col-7 font-weight-bold p-3 mb-2 bg-secondary text-white">
         Description
       </div>
-      <div class="col-1 font-weight-bold p-3 mb-2 bg-secondary text-white">
+      <div class="col-1 font-weight-bold p-3 mb-2 bg-secondary text-white text-left">
         Hours
       </div>
-      <div class="col-1 font-weight-bold p-3 mb-2 bg-secondary text-white">
+      <div class="col-1 font-weight-bold p-3 mb-2 bg-secondary text-white text-left">
         Rate
       </div>
-      <div class="col-1 font-weight-bold p-3 mb-2 bg-secondary text-white">
+      <div class="col-1 font-weight-bold p-3 mb-2 bg-secondary text-white text-left" >
         Total
       </div>
       <div
-        class="col-2 font-weight-bold p-3 mb-2 bg-secondary text-white text-left"
+        class="col-2 font-weight-bold p-3 mb-2 bg-secondary text-white text-center"
       >
         Actions
       </div>
@@ -22,29 +22,35 @@
     <br />
     <div :key="index" v-for="(task,index) in tasks">
       <div class="container-fluid">
-    <div class="row">
-      <div class="col-1 border-bottom">
-      
+        <div class="row">
+          <div class="col-1 border-bottom">
+          
+          </div>
+          <div class="col-6 text-left border-bottom">
+            <p>Task title: {{task.title}}</p>
+            <p>Task description: {{ task.description }}</p>
+          </div>
+        
+          <div class="col-1 text-left border-bottom">
+            Hours
+          </div>
+          <div class="col-1 text-left border-bottom">
+            Rate
+          </div>
+          <div class="col-1 text-left border-bottom">
+            Total
+          </div>
+          <div class="col-1 text-left border-bottom">
+            <button @click="editTasks" class="btn btn-success">
+              Edit
+            </button>
+          </div>
+          <div class="col-1 text-left border-bottom">
+            <button @click="deleteTasks" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
       </div>
-      <div class="col-6 text-left border-bottom">
-        <p>Task title: {{task.title}}</p>
-        <p>Task description: {{ task.description }}</p>
-      </div>
-      <div class="col-1 text-left border-bottom">
-        Hours
-      </div>
-      <div class="col-1 text-left border-bottom">
-        Rate
-      </div>
-      <div class="col-1 text-left border-bottom">
-        Total
-      </div>
-      <div
-        class="col-2 text-left border-bottom"
-      >
-        Actions
-      </div>
-    </div>
     <br />
   </div>
     </div>
